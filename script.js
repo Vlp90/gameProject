b8ca8c9ffafe64dade31aded633c658e6244f495
vlp90 <vlp90@DESKTOP-VUTPP2C.localdomain>
2020-04-07 10:38:11 +0200

    JS begin

---
+++ script.js
@@ -0,0 +1,21 @@
+const addPlayerBtn = document.getElementById('addPlayer');
+const blockPlayerContainer = document.getElementById('player-select order order3')
+
+
+// ADD PLAYER CONTAINER
+
+function addPlayerContainer() {
+
+
+//     const newBlock = document.createElement("div");
+//   // console.dir(newBlock, "this is my new div");
+//   newBlock.classList.add("player-select");
+
+//   return newBlock;
+
+    addPlayerBtn.style.backgroundColor = "red";
+    addPlayerBtn.style.color = "white";
+    addPlayerBtn.innerHTML = "Red vlad"; 
+}
+
+addPlayerBtn.onclick = addPlayerContainer;
