6318ae3ab48722f299cd55c8ef7483fd3e16ea29
vlp90 <vlp90@DESKTOP-VUTPP2C.localdomain>
2020-04-07 16:00:36 +0200

    Link between pages done

---
+++ board/board.js
@@ -0,0 +1,15 @@
+import { players } from '../script.js';
+import { colors } from '../script.js';
+
+// console.log("vlad")
+
+// console.log(players);
+// console.log(colors);
+
+let player1 = {
+    name : "players",
+    color : "colors"
+};
+
+console.log(player1)
+
